7e76b44273c5afe25952a24188f65380777ecb18
Jordi Clariana <jordi.clariana@ankorstore.com>
2024-02-02 12:57:58 +0100

    fix: fixing
    Signed-off-by: Jordi Clariana <jordi.clariana@ankorstore.com>

--- dist/index.js
+++ dist/index.js
@@ -37000,7 +37000,7 @@ module.exports = async function run() {
         repo,
         sha: pullRequest.head.sha,
         state: newStatus,
-        target_url: 'https://github.com/amannn/action-semantic-pull-request',
+        target_url: 'https://github.com/jordiclariana/action-semantic-pull-request',
         description: isWip
           ? 'This PR is marked with "[WIP]".'
           : validationError
